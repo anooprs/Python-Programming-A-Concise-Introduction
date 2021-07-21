cdfac9c3b91062e45a1dbd288689c40b51b0d110
Anoop Remanan Syamala <53828695+anooprs@users.noreply.github.com>
2021-07-21 19:17:23 +0530

    Add files via upload

---
+++ Week 3/problemSet3_5.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 19:12:43 2021
+
+@author: anoop
+"""
+
+#%%
+"""    
+    
+Problem 3_5:
+Write a function that will look up a phone number given a name.  Use this
+dictionary of phone numbers in your program, so that the grader will know 
+what phone numbers are available.  In it's simplest form, the program will
+crash if a name that isn't in its dictionary is asked for.
+Here is a printout of one of my runs.
+problem3_5("james")
+(212) 567-8149
+Below is the start of the program including the dictionary.
+"""
+#%%
+
+def problem3_5(name):
+    """ Looks up the phone number of the person whose name is name """
+    
+    phone_numbers = {"abbie":"(860) 123-4535", "beverly":"(901) 454-3241", \
+                      "james": "(212) 567-8149", "thomas": "(795) 342-9145"}
+    pass # replace this pass (a do-nothing) statement with your code
+    
+    dictionary = (phone_numbers[name])        
+    print(dictionary)
+
+#%%
